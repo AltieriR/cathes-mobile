bc95d61840a2e6484df2ff6fe334fdfb24f640b6
Altieri R. Dalagnollo <altidala@hotmail.com>
2019-07-01 21:54:41 -0300

    Edit equipment modal setup

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Image, ActivityIndicator, ScrollView } from 'react-native';
-import { Button } from '@ant-design/react-native';
+import { Platform, StyleSheet, Text, View, Image, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
+import { Button, InputItem, Modal, Provider } from '@ant-design/react-native';
 
 export default class Welcome extends Component {
   constructor(props) {
     super(props);
+
+    this.onClose = () => {
+      this.setState({
+        visible: false,
+      });
+    };
+
+    this.onButtonClick = () => {
+      Modal.alert('Title', 'alert content', [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('cancel'),
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => console.log('ok') },
+      ]);
+    };
+
     this.state = {
       isLoading: true,
       dataSource: null,
+      visible: false,
     }
   }
 
@@ -31,6 +50,12 @@ export default class Welcome extends Component {
   }*/
 
   render() {
+
+    const footerButtons = [
+      { text: 'Cancel', onPress: () => console.log('cancel') },
+      { text: 'Ok', onPress: () => console.log('ok') },
+    ];
+
     if (this.state.isLoading) {
       return (
         <View style={styles.container}>
@@ -39,15 +64,19 @@ export default class Welcome extends Component {
       );
     } else {
       let obj = this.state.dataSource.map((val, key) => {
-        return <View key={key}>
+        return <TouchableOpacity key={key}
+
+          onPress={() => this.setState({ visible: true })}
+        >
           <View style={{ flex: 1, flexDirection: 'row', padding: 5 }}>
             <Image
               style={{ padding: 15, width: 50, height: 50, borderRadius: 50, overflow: 'hidden', borderWidth: 3, borderColor: 'black' }}
-              source={{ uri: 'http://10.0.2.2:3000/' +val.image.replace(/\\/g, '/')}}
+              source={{ uri: 'http://10.0.2.2:3000/' + val.image.replace(/\\/g, '/') }}
             />
             <View style={{ flex: 1, flexDirection: 'column', paddingStart: 10 }}>
               <Text>{val.name}</Text>
               <Text>{val.campus}</Text>
+
             </View>
           </View>
           <View
@@ -56,28 +85,54 @@ export default class Welcome extends Component {
               borderBottomWidth: 1,
             }}
           />
-        </View>
+        </TouchableOpacity>
       });
 
       const { navigate } = this.props.navigation;
       return (
-        <ScrollView
-          style={{ flex: 1, backgroundColor: '#f5f5f9' }}
-          automaticallyAdjustContentInsets={false}
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-        >
+        <Provider>
+          <ScrollView
+            style={{ flex: 1, backgroundColor: '#f5f5f9' }}
+            automaticallyAdjustContentInsets={false}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+          >
 
-          <View
-            style={{
-              borderBottomColor: '#5e5e5e',
-              borderBottomWidth: 1,
-            }}
-          />
-          {obj}
-          <Button onPress={() => navigate('Welcome', { name: 'Testing' })}>Go back</Button>
+            <Modal
+              title="Editar Equipamento"
+              transparent
+              onClose={this.onClose}
+              maskClosable
+              visible={this.state.visible}
+              closable
+              footer={footerButtons}
+            >
+              <View style={{ paddingVertical: 20 }}>
 
-        </ScrollView>
+                <Text style={{ textAlign: 'center' }}>Equipment:</Text>
+                <InputItem
+                  clear
+                  defaultValue="Robot"
+                />
+                <Text style={{ textAlign: 'center' }}>Description:</Text>
+                <InputItem
+                  clear
+                  defaultValue="Metal boxer"
+                />
+              </View>
+            </Modal>
+
+            <View
+              style={{
+                borderBottomColor: '#5e5e5e',
+                borderBottomWidth: 1,
+              }}
+            />
+            {obj}
+            <Button onPress={() => navigate('Welcome', { name: 'Testing' })}>Go back</Button>
+
+          </ScrollView>
+        </Provider>
       );
     }
   }
